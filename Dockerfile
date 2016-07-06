ddc8a304e912df28f91ef318c5b6b3336aaf9c51
vandene <erikvdbergh@gmail.com>
2016-07-06 11:29:57 +0100

    correct script to take files as list

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ubuntu:16.04
 
+MAINTAINER Erik van den Bergh, Earlham Institute, Norwich
+
 RUN apt update
 RUN apt install -y wget rsync net-tools
 
@@ -11,14 +13,20 @@ RUN chown admin:admin /opt/smrtanalysis
 USER admin
 WORKDIR /home/admin
 
-RUN wget http://files.pacb.com/software/smrtanalysis/2.3.0/smrtanalysis_2.3.0.140936.run
-RUN wget https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.3.0/smrtanalysis-patch_2.3.0.140936.p5.run
+RUN wget -nv http://files.pacb.com/software/smrtanalysis/2.3.0/smrtanalysis_2.3.0.140936.run
+RUN wget -nv https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.3.0/smrtanalysis-patch_2.3.0.140936.p5.run
 
 RUN bash smrtanalysis_2.3.0.140936.run -p smrtanalysis-patch_2.3.0.140936.p5.run --rootdir /opt/smrtanalysis --ignore-syscheck --jmstype NONE --batch
 
 RUN rm -rf *
 
-COPY scripts /home/admin
-COPY templates /home/admin
+RUN mkdir /home/admin/scripts
+COPY scripts /home/admin/scripts
+
+USER root
+
+RUN chown admin:admin /home/admin/scripts/*
+
+USER admin
 
 ENTRYPOINT ["/home/admin/scripts/runRenSeq.sh"]
--- scripts/runRenSeq.sh
+++ scripts/runRenSeq.sh
@@ -5,13 +5,19 @@
 
 . $("/opt/smrtanalysis/admin/bin/getsetupfile")
 
-if [ "$#" -ne 2 ]; then
-  echo -e "\nUsage:\trunRenSeq.sh path/to/data adapter.fasta\n"
+basedir=$(dirname $0)
+
+if [ "$#" -lt 2 ]; then
+  echo -e "\nUsage:\trunRenSeq.sh adapter.fasta file1.h5 file2.h5 ...\n"
   exit
 fi
 
-DATADIR=$1
-ADAPTER=$2
+ADAPTER=$1
+
+ARGV=("$@")
+NUMFILES=${#ARGV[@]}
+
+#echo [`date`] processing $NUMFILES data files
 
 echo [`date`] Making directories 1-blasr/raw and 1-blasr/trimmed
 mkdir -p 1-blasr/{raw,trimmed}
@@ -25,18 +31,20 @@ mkdir -p 1-blasr/{raw,trimmed}
 #done
 
 echo [`date`] Finding trim length...
-trim_by=`python 3-find_trim_by.py $ADAPTER`
+trim_by=`python $basedir/3-find_trim_by.py $ADAPTER`
 echo [`date`] Using trim length $trim_by
 
 echo [`date`] Trimming adapters from h5 files...
-for raw in $DATADIR/*; do
+for (( i=1; $i<${NUMFILES}; i++ )) ; do
+  raw=${ARGV[$i]}
   echo [`date`] Trimming file $raw...
-  python 4-edit-h5.py $trim_by $raw
+  python $basedir/4-edit-h5.py $trim_by $raw
 done
 echo [`date`] Trimmed adapters from files
 
 echo [`date`] Running blasr on trimmed files...
-for raw in $DATADIR/*; do
+for (( i=1; i<${NUMFILES}; i++ )) ; do
+  raw=${ARGV[$i]}
   echo [`date`] Running blasr on $raw
   rawbase=`basename $raw`
   blasr $raw $ADAPTER -m 1 -bestn 1 -out 1-blasr/trimmed/$rawbase.m4
@@ -55,18 +63,19 @@ echo [`date`] Extracting whitelist...
 for raw in 1-blasr/trimmed/*.m4; do
   echo [`date`] Extracting whitelist from $raw
   rawbase=`basename $raw`
-  python 7-whitelist.py $raw 54494 > $rawbase.whitelist
+  python $basedir/7-whitelist.py $raw 54494 > $rawbase.whitelist
   WHITELIST=$rawbase.whitelist
 done
 
 echo [`date`] inserting parameters and generating params.xml
-python insert_params.py params_v5.xml `pwd`/$WHITELIST > params.xml
+python $basedir/insert_params.py $basedir/params_v5.xml `pwd`/$WHITELIST > params.xml
 
 echo [`date`] generating input.xml
 rm input.fofn
 
-for raw in $DATADIR/*; do
-  echo `pwd`/$raw >> input.fofn
+for (( i=1; i<${NUMFILES}; i++ )) ; do
+  raw=${ARGV[$i]}
+  echo $raw >> input.fofn
 done
 
 fofnToSmrtpipeInput.py input.fofn > input.xml
