161fd7505d37c15e6697b46960c999b8213536a0
Erik van den Bergh <erikvdbergh@gmail.com>
2016-10-21 15:46:53 +0100

    correct quotes leading to error

--- scripts/insert_params.py
+++ scripts/insert_params.py
@@ -4,6 +4,6 @@ from xml.etree.ElementTree import ElementTree, dump
 tree = ElementTree()
 tree.parse(sys.argv[1])
 
-tree.find('module/param[@name='+sys.argv[2]+']')[0].text = sys.argv[3]
+tree.find('module/param[@name=\"'+sys.argv[2]+'\"]')[0].text = sys.argv[3]
 
 dump(tree)
